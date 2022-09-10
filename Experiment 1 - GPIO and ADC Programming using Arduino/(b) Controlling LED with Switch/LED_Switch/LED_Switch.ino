bacdff0ab40d83faabb2f27243c3b5a9e76e9bc8
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-09-10 22:59:52 +0530

    Create LED_Switch.ino

---
+++ Experiment 1 - GPIO and ADC Programming using Arduino/(b) Controlling LED with Switch/LED_Switch/LED_Switch.ino
@@ -0,0 +1,31 @@
+/*
+  Switch:
+*/
+
+// This constant won't change:
+const int digitalPin = 2; // Pin that the digital pin is attached to
+const int ledPin = 13; // Pin that the LED is attached to
+int digitalValue = 0;
+
+// The setup function runs once when you press reset or power the board: 
+void setup()
+{ 
+  pinMode(digitalPin, INPUT); // Initialize the digital pin as an input
+  pinMode(ledPin, OUTPUT); // Initialize the LED pin as an output
+}
+
+// The loop function runs over and over again forever:
+void loop()
+{
+  digitalValue = digitalRead(digitalPin); // Read the value
+
+  // If the digital value is equal to 2, turn on the LED:
+  if (digitalValue == HIGH)
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin, LOW);
+  }  
+} 
