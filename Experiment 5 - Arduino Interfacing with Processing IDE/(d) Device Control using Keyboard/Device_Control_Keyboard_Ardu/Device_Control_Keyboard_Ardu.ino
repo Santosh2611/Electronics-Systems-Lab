02197e27ff366547be6cb9bc9e1fb364fafb42c9
Santosh <88974296+Santosh2611@users.noreply.github.com>
2022-09-10 23:39:11 +0530

    Create Device_Control_Keyboard_Ardu.ino

---
+++ Experiment 5 - Arduino Interfacing with Processing IDE/(d) Device Control using Keyboard/Device_Control_Keyboard_Ardu/Device_Control_Keyboard_Ardu.ino
@@ -0,0 +1,30 @@
+char val; // Variable to store the data received from the serial port
+int ledPin = 13; // LED Pin of Arduino
+
+// The setup function runs once when you press reset or power the board:
+void setup()
+{
+  pinMode(ledPin, OUTPUT); // Set pin D13 (RED-LED) as output
+  Serial.begin(9600); // Start serial communications at 9,600 bits per second
+}
+
+// The loop function runs over and over again forever:
+void loop()
+{
+  if ( Serial.available() )
+  {
+    // ID data is available to read now:
+    val = Serial.read(); // Read the value and store it in a variable "val"
+  }
+  
+  if (val == '1')
+  {
+    digitalWrite(ledPin, HIGH); // Turn ON Arduino LED
+  }
+  else if (val == '0') 
+  {
+    digitalWrite(ledPin, LOW); // Turn OFF Arduino LED
+  }
+  
+  delay(10); // Delay in between reads for stability
+}
